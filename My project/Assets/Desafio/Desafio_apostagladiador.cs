3f896b97aa10e4dd45b5e5e46064f500ce8645cf
Rhaziel <rhazicoelho@gmail.com>
2026-04-07 17:45:12 -0300

    desafio gladiador terminado

--- My project/Assets/Desafio/Desafio_apostagladiador.cs
+++ My project/Assets/Desafio/Desafio_apostagladiador.cs
@@ -2,55 +2,63 @@ using UnityEngine;
 
 public class Desafio_apostagladiador : MonoBehaviour
 {
-    [SerializeField] private int moedasjogador = 100;
+    private int moedasjogador = 100;
     [SerializeField] private int apostagladiadorA;
     [SerializeField] private int apostagladiadorB;
     private int forcagladiadorA;
     private int forcagladiadorB;
-    private bool jogando;
+    [SerializeField] private bool jogando;
     void Start()
     {
-        print("selecione um gladiador para apostar");
+        print("selecione um gladiador para apostar, você tem 100 peças de ouro");
         print("confirme sua seleção com E");
+        
+    }
+
+
+    void Update()
+    {              
+            if (Input.GetKeyDown(KeyCode.E) && jogando == true)
+            { 
+             
+             if (apostagladiadorA > moedasjogador || apostagladiadorB > moedasjogador && jogando == true)
+             {
+              print("você não tem moedas suficientes");
+                jogando = false;
+             }
+                forcagladiadorA = Random.Range(1, 101);
+                forcagladiadorB = Random.Range(1, 101);
+                if (forcagladiadorA > forcagladiadorB && jogando == true)
+                {
+                    print("Gladiador A é mais forte e vence.");
+                    apostagladiadorA = apostagladiadorA * 2;
+                    moedasjogador = moedasjogador + apostagladiadorA;
+                    moedasjogador = moedasjogador - apostagladiadorB;
+                    print("você tem " + moedasjogador + " peças de ouro.");
+                }
+
+
+            else if (forcagladiadorB > forcagladiadorA && jogando == true)
+            {
+                print("gladiador B é mais forte e vence.");
+                apostagladiadorB = apostagladiadorB * 2;
+                moedasjogador = moedasjogador + apostagladiadorB;
+                moedasjogador = moedasjogador - apostagladiadorA;
+                print("você tem" + moedasjogador + " peças de ouro");
+            }
+                if (apostagladiadorA == apostagladiadorB)
+                {
+                print("empate, nada acontece.");
+                }
+                         
+                      
+            }
+        else if (Input.GetKeyDown(KeyCode.E) && jogando == false)
+        {
+            print("esteja jogando para apostar.");
+        }
+
     }
 
     
-    void Update()
-    {
-        if (apostagladiadorA > moedasjogador || apostagladiadorB > moedasjogador)
-        {
-            print("você não tem moedas suficientes");
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                forcagladiadorA = Random.Range(1, 101);
-                forcagladiadorB = Random.Range(1, 101);
-                if (forcagladiadorA > forcagladiadorB)
-                {
-                    print("Gladiador A é mais forte e vence.");
-                    apostagladiadorA = apostagladiadorB * 2;
-                    moedasjogador = moedasjogador + apostagladiadorA;
-                }
-                else
-                {
-                    print("gladiador B é mais forte e vence.");
-                    moedasjogador = moedasjogador - apostagladiadorA;
-                }
-                if (forcagladiadorA < forcagladiadorB)
-                {
-                    print("gladiador B é mais forte e vence.");
-                    apostagladiadorB = apostagladiadorB * 2;
-                    moedasjogador = moedasjogador + apostagladiadorB;
-                }
-                else
-                {
-                    print("gladiador A é mais forte e vence");
-                    moedasjogador = moedasjogador - apostagladiadorB;
-                }
-            }
-        }
-
-    }
 }
